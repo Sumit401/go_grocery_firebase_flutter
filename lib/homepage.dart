7bcaa9f8c362f4b563251986cd5bd939e01c2f73
Sumit <sumitsinha401@gmail.com>
2022-12-18 13:29:58 +0530

    Version 1.0

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,6 @@
-import 'package:cabs/login.dart';
 import 'package:cabs/bottom_navbar.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
@@ -12,6 +11,7 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
+  var fire_storedb= FirebaseFirestore.instance.collection("cars").snapshots();
 
 
   @override
@@ -20,13 +20,38 @@ class _homepageState extends State<homepage> {
     return Scaffold(
       bottomNavigationBar: bottom_navbar(),
       body: Container(
-        child: ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              Navigator.pop(context);
-              Navigator.pushNamed(context, login.route);
-            },
-            child: Text("Logout")),
+        margin: EdgeInsets.only(top: 20),
+        alignment: Alignment.center,
+        child: StreamBuilder(stream: fire_storedb,builder: ((context, snapshot) {
+          if(!snapshot.hasData) return CircularProgressIndicator();
+          return(
+          ListView.builder(itemCount: snapshot.data!.docs.length,itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("Name of the vehicle"),
+                    Text(snapshot.data!.docs[index]['Name']),
+                  ],
+                ),
+                Text(snapshot.data!.docs[index]['Fuel Type']),
+                Text(snapshot.data!.docs[index]['Mileage']),
+                Text(snapshot.data!.docs[index]['Safety']),
+                Text(snapshot.data!.docs[index]['Seating Capacity']),
+                Image.network(snapshot.data!.docs[index]['image'].toString(),width: 300,height: 300,)
+              ],
+            );
+          },)
+          );
+        })),
+        /* ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, login.route);
+                },
+                child: Text("Logout")),*/
       ),
     );
   }
